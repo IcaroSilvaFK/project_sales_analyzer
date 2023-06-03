158bd3a5728f2ede47a214109ab6640bcf307fd1
Icaro Vieira <icarovsilva1@gmail.com>
2023-06-03 14:43:06 -0300

    feat: init

---
+++ features/components/CHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <Header>
+    <h1>Icaro Vieira</h1>
+  </Header>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <CHeader />
+</template>
+
+<script setup lang="ts">
+  import CHeader from '../features/components/CHeader.vue';
+  
+
+</script>
+
+
+<style scoped lang="scss">
+</style>
